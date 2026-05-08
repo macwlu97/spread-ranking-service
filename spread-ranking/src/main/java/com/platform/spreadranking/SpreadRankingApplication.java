bf4319c64ba5e84dcc3f461cbf4c034debae0e7d
macwlu97 <maciej.wnuklipinski@gmail.com>
2026-05-08 13:02:19 +0200

    refactor: reorganize codebase into dedicated spread-ranking module and remove legacy structure

---
+++ spread-ranking/src/main/java/com/platform/spreadranking/SpreadRankingApplication.java
@@ -0,0 +1,11 @@
+package com.platform.spreadranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadRankingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpreadRankingApplication.class, args);
+    }
+}
